c772364e56ba6f4e24cefcab482e905422b72485
chokawaichan <ccc679253@gmail.com>
2025-12-22 15:56:21 +0900

    style: 로딩 메시지를 제주 숨곳 컨셉에 맞게 변경

--- project/svelte_template/src/components/LoadingSpinner.svelte
+++ project/svelte_template/src/components/LoadingSpinner.svelte
@@ -1,6 +1,6 @@
 <div class="flex flex-col items-center justify-center p-8">
     <div
-        class="w-12 h-12 border-4 border-gray-200 border-t-blue-500 rounded-full animate-spin"
+        class="w-12 h-12 border-4 border-gray-200 border-t-indigo-500 rounded-full animate-spin"
     ></div>
-    <p class="mt-4 text-gray-500 text-sm">AI가 응답하는 중입니다...</p>
+    <p class="mt-4 text-gray-500 text-sm">백록이가 숨은곳을 찾고 있어요...</p>
 </div>
